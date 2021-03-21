0e80ca1e85558ad6763a0f962b1797fe088ea25a
wslusarczyk <wioletkaslusarczyk@wp.pl>
2021-03-21 23:27:49 +0100

    Poprawione literowki

--- main.py
+++ main.py
@@ -3,53 +3,58 @@
  2)kwadratu
 '''
 
-def rectangle(lenght,width):
-    if lenght == 0 or width == 0:
+
+def rectangle(length, width):
+    if length == 0 or width == 0:
         return
     else:
-        areaRectangle = lenght * width
+        areaRectangle = length * width
         return areaRectangle
 
-def checkRectangleValue(lenght, width):
-    while lenght == 0 or width == 0:
-        print("Your value cannot be zero!")
-        lenght = int(input("Enter the lenght = "))
-        width = int(input("Enter the width = "))
-    print("Area rectangle = ", rectangle(lenght,width))
 
-def square(lenghtSquare):
-    if lenghtSquare == 0:
+def checkRectangleValue(length, width):
+    while length == 0 or width == 0:
+        print("Your value cannot be zero!")
+        length = int(input("Enter the length = "))
+        width = int(input("Enter the width = "))
+    print("Area of rectangle = ", rectangle(length, width))
+
+
+def square(lengthSquare):
+    if lengthSquare == 0:
         return
     else:
-        areaSquare = lenghtSquare**2
+        areaSquare = lengthSquare ** 2
         return areaSquare
 
-def checkSquareValue(lenghtSquare):
-    while lenghtSquare == 0:
+
+def checkSquareValue(lengthSquare):
+    while lengthSquare == 0:
         print("Your value cannot be zero!")
-        lenghtSquare = int(input("Enter the lenghtSquare = "))
-    print("Area rectangle = ", square(lenghtSquare))
+        lengthSquare = int(input("Enter the lenghtSquare = "))
+    print("Area of square = ", square(lengthSquare))
 
-print("A program that counts the area of a selected figure")
 
-def userChoise():
-    choise = int(input("Choise one option \n 1) Rectangle area \n 2) Square area \n 3) Finish \n"))
-    return choise
+def userChoice():
+    choice = int(input("Choose one \n 1) Rectangle area \n 2) Square area \n 3) Finish \n"))
+    return choice
 
-choise = int(input("Choise one option \n 1) Rectangle area \n 2) Square area \n 3) Finish \n"))
 
-'''while choise != 1 and choise != 2 and choise !=3:
-    choise = int(input("Choise one option \n 1) Rectangle area \n 2) Square area \n 3) Finish \n"))'''
-while choise !=3:
-    if choise == 1:
-        lenght = int(input("Enter the lenght = "))
+print("A program that calculates the area of a selected figure")
+
+choice = userChoice()
+
+while choice != 3:
+    if choice == 1:
+        length = int(input("Enter the length = "))
         width = int(input("Enter the width = "))
-        checkRectangleValue(lenght, width)
-    elif choise == 2:
-        lenghtSquare = int(input("Enter the lenght = "))
-        checkSquareValue(lenghtSquare)
-    elif choise == 3:
+        checkRectangleValue(length, width)
+    elif choice == 2:
+        lengthSquare = int(input("Enter the length = "))
+        checkSquareValue(lengthSquare)
+    elif choice == 3:
         print("Finish")
+        exit()
     else:
         print("The given value is not valid! Choose the correct number ")
-    choise = int(input("Choise one option \n 1) Rectangle area \n 2) Square area \n 3) Finish \n"))
+    choice = userChoice()
